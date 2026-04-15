5a2edb4723b598a2199960943d760e82df027513
Amine Nahli <nahli-ami@upf.ac.ma>
2026-04-15 15:14:57 +0100

    feat: integrate Tailwind CSS and Framer Motion with project initialization scripts

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
->>>>>>> parent of 159940c (1er prototype)
 import App from './App.jsx'
+import './styles/index.css'
 
-<<<<<<< HEAD
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-=======
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
->>>>>>> parent of 159940c (1er prototype)
 )
